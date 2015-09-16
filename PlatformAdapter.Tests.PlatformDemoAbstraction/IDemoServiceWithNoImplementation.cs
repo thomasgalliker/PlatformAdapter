28562bf3312493f2a61b18dde799879342d8e506
thomasgalliker <thomas@bluewin.ch>
2015-09-16 18:48:05 +0200

    Added missing files

---
+++ PlatformAdapter.Tests.PlatformDemoAbstraction/IDemoServiceWithNoImplementation.cs
@@ -0,0 +1,10 @@
+﻿namespace PlatformAdapter.Tests.PlatformDemoAbstraction
+{
+    /// <summary>
+    /// This is a demo service interface which is placed in a platform-agnostic assembly.
+    /// For testing purposes, there is no implementation available in the plaform-specific assembly.
+    /// </summary>
+    public interface IDemoServiceWithNoImplementation
+    {
+    }
+}
---
+++ PlatformAdapter.Tests.PlatformDemoAssembly/DemoService.cs
@@ -0,0 +1,11 @@
+using PlatformAdapter.Tests.PlatformDemoAbstraction;
+
+namespace PlatformAdapter.Tests.PlatformDemoAssembly
+{
+    /// <summary>
+    /// This is a demo service implementation which is placed in a platform-specific assembly.
+    /// </summary>
+    public class DemoService : IDemoService
+    {
+    }
+}
