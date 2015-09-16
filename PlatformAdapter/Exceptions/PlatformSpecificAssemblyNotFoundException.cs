eb6d982dbf08a5489c5a5daef9f7433fd4a26485
thomasgalliker <thomas@bluewin.ch>
2015-09-16 15:14:01 +0200

    Added missing files

---
+++ PlatformAdapter/Exceptions/PlatformSpecificAssemblyNotFoundException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PlatformAdapter.Exceptions
+{
+    public class PlatformSpecificAssemblyNotFoundException : Exception
+    {
+        public PlatformSpecificAssemblyNotFoundException()
+        {
+        }
+    
+        public PlatformSpecificAssemblyNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        public PlatformSpecificAssemblyNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
---
+++ PlatformAdapter/IAdapterResolver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PlatformAdapter
+{
+    public interface IAdapterResolver
+    {
+        object Resolve(Type interfaceType, bool throwIfNotFound, object[] args);
+
+        /// <summary>
+        /// Resolves a class type based on the given interface type.
+        /// </summary>
+        /// <param name="interfaceType">The interface type for which the implementation type is looked up.</param>
+        /// <param name="throwIfNotFound">Throws an exception if class type cannot be found.</param>
+        /// <returns>The class type (if it can be found) or null (if it cannot be found and throwIfNotFound is false).</returns>
+        Type ResolveClassType(Type interfaceType, bool throwIfNotFound = true);
+
+        IRegistrationConvention RegistrationConvention { get; set; }
+    }
+}
