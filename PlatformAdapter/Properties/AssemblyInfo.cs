b4660c6d03be4e90be52b2c306c5b27c40092cd9
thomasgalliker <thomas@bluewin.ch>
2016-01-07 23:27:14 +0100

    Update project details (nuspec + AssemblyInfo)

--- PlatformAdapter/Properties/AssemblyInfo.cs
+++ PlatformAdapter/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PlatformAdapter")]
-[assembly: AssemblyCopyright("Copyright © 2015")]
+[assembly: AssemblyCopyright("Copyright © 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
